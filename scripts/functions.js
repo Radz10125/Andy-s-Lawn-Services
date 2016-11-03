4d8d8f9b37bbf33f1530e23f06802e29278de721
Andrew Radloff <andy.radloff@gmail.com>
2016-11-03 17:49:21 -0500

    Completed Homepage Design

---
+++ scripts/functions.js
@@ -0,0 +1,21 @@
+$(function() {
+  mobileNav();
+});
+
+
+function mobileNav() {
+  $('.mobile-nav-toggle').on('click', function(){
+    var status = $(this).hasClass('is-open');
+    if(status){ $('.mobile-nav-toggle, .mobile-nav').removeClass('is-open'); }
+    else { $('.mobile-nav-toggle, .mobile-nav').addClass('is-open'); }
+  });
+}
+
+$(window).scroll(function(){
+  var wScroll = $(this).scrollTop();
+  var heightRatio = $(window).height();
+
+  $('.logo').css({
+    'transform': 'translate(0px, '+ wScroll/(heightRatio/290) +'%)'
+  });
+});
